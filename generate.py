c9fa31e75f6dcee21c545d7a8c0c79f8725c4f95
Your Name <you@example.com>
2023-11-14 19:55:32 +0300

    2 spaces to tabs and fix login

--- generate.py
+++ generate.py
@@ -6,40 +6,40 @@ import random
 import json
 
 def get_neighboor(map, x, y, dx, dy):
-    #print(map)
-    if dx < 0 and x == 0:
-        return None
-    if dx == 1 and x == (len(map[0])-1):
-        return None
-    if dy < 0 and y == 0:
-        return None
-    if dy == 1 and y == (len(map[0]))-1:
-        return None
-    try:
-        return map[y+dy][x+dx]
-    except:
-        print(y+dy, x+dx, dx,)
-        raise ZeroDivisionError
+		#print(map)
+		if dx < 0 and x == 0:
+				return None
+		if dx == 1 and x == (len(map[0])-1):
+				return None
+		if dy < 0 and y == 0:
+				return None
+		if dy == 1 and y == (len(map[0]))-1:
+				return None
+		try:
+				return map[y+dy][x+dx]
+		except:
+				print(y+dy, x+dx, dx,)
+				raise ZeroDivisionError
 
 
 
 def merge_chunks(c1, c2, horizontal):
-    result = []
-    if horizontal:
-        for i in range(len(c1)):
-            result.append([*c1[i], *c2[i]])
-    else:
-        result = [*c1, *c2]
-    return result
+		result = []
+		if horizontal:
+				for i in range(len(c1)):
+						result.append([*c1[i], *c2[i]])
+		else:
+				result = [*c1, *c2]
+		return result
 
 def _3on3():
-    result = []
-    for i in range(3):
-        row = []
-        for j in range(3):
-            row.append(Chunk.generate_chunk(j,i))
-        result.append(merge_chunks(merge_chunks(json.loads(row[0].content), json.loads(row[1].content), True), json.loads(row[2].content), True))
-    return merge_chunks(merge_chunks(result[0], result[1], False), result[2], False)
+		result = []
+		for i in range(3):
+				row = []
+				for j in range(3):
+						row.append(Chunk.generate_chunk(j,i))
+				result.append(merge_chunks(merge_chunks(json.loads(row[0].content), json.loads(row[1].content), True), json.loads(row[2].content), True))
+		return merge_chunks(merge_chunks(result[0], result[1], False), result[2], False)
 
 """a = _3on3()
 f = open('landscaft.json', 'w')
--- proxies.py
+++ proxies.py
@@ -3,119 +3,119 @@ import models
 import json
 
 """class JSONProxy:
-    def __init__(self, dict_={}):
-        self.dict = dict_
-    def __getattr__(self, name):
-        if name == "dict":
-            return self.dict
-        elif name in self.dict.keys():
-            attr = self.dict[name]
-            if type(attr) == dict:
-                return JSONProxy(attr)
-            else:
-                return attr
-        else:
-            return None
-    def __setattr__(self, name, value):
-        if name == "dict":
-            self.dict = value
-            return
-        self.dict[name] = value
+		def __init__(self, dict_={}):
+				self.dict = dict_
+		def __getattr__(self, name):
+				if name == "dict":
+						return self.dict
+				elif name in self.dict.keys():
+						attr = self.dict[name]
+						if type(attr) == dict:
+								return JSONProxy(attr)
+						else:
+								return attr
+				else:
+						return None
+		def __setattr__(self, name, value):
+				if name == "dict":
+						self.dict = value
+						return
+				self.dict[name] = value
 """
 
 class PlayerProxy:
-    def __init__(self, model):
-        self.model_link = model
-    def sync_from(self):
-        self.state = json.loads(self.model_link.state)
-        self.appearance = json.loads(self.model_link.appearance)
-    def sync_to(self):
-        self.model_link.state = json.dumps(self.state)
-        self.model_link.appearance = json.dumps(self.appearance)
-    def json(self):
-        return {
-            'type': 'player',
-            'id': self.model_link.id,
-            'name': self.model_link.nickname,
-            'appearence': self.appearance,
-            'state': self.state
-        }
+		def __init__(self, model):
+				self.model_link = model
+		def sync_from(self):
+				self.state = json.loads(self.model_link.state)
+				self.appearance = json.loads(self.model_link.appearance)
+		def sync_to(self):
+				self.model_link.state = json.dumps(self.state)
+				self.model_link.appearance = json.dumps(self.appearance)
+		def json(self):
+				return {
+						'type': 'player',
+						'id': self.model_link.id,
+						'name': self.model_link.nickname,
+						'appearence': self.appearance,
+						'state': self.state
+				}
 
 class StaticEntityProxy:
-    def __init__(self, model):
-        self.model_link = model
-    def sync_from(self):
-        self.statedata = json.loads(self.model_link.state)
-        self.appearancedata = json.loads(self.model_link.appearance)
-    def sync_to(self):
-        self.model_link.state = json.dumps(self.statedata)
-        self.model_link.appearance = json.dumps(self.appearancedata)
-    def json(self):
-        return {
-            'id': self.model_link.id,
-            'type': self.model_link.type,
-            'appearance': self.appearancedata,
-            'state': self.statedata
-        }
+		def __init__(self, model):
+				self.model_link = model
+		def sync_from(self):
+				self.statedata = json.loads(self.model_link.state)
+				self.appearancedata = json.loads(self.model_link.appearance)
+		def sync_to(self):
+				self.model_link.state = json.dumps(self.statedata)
+				self.model_link.appearance = json.dumps(self.appearancedata)
+		def json(self):
+				return {
+						'id': self.model_link.id,
+						'type': self.model_link.type,
+						'appearance': self.appearancedata,
+						'state': self.statedata
+				}
 
 class ItemProxy:
-    def __init__(self, model):
-        self.model_link = model
-    def sync_from(self):
-        self.description = json.loads(self.model_link.description)
-        self.state = json.loads(self.model_link.state)
-    def sync_to(self):
-        self.model_link.description = json.dumps(self.description)
-        self.model_link.state = json.self.state(self.state)
-    def json(self):
-        return {
-            "id": self.model_link.id,
-            "name": self.model_link.name,
-            "category": self.model_link.category,
-            "description": self.description,
-            "state": self.state
-        }
+		def __init__(self, model):
+				self.model_link = model
+		def sync_from(self):
+				self.description = json.loads(self.model_link.description)
+				self.state = json.loads(self.model_link.state)
+		def sync_to(self):
+				self.model_link.description = json.dumps(self.description)
+				self.model_link.state = json.self.state(self.state)
+		def json(self):
+				return {
+						"id": self.model_link.id,
+						"name": self.model_link.name,
+						"category": self.model_link.category,
+						"description": self.description,
+						"state": self.state
+				}
 
 class ItemPool:
-    pool = dict()
-    session = models.async_session()
+		pool = dict()
+		session = models.async_session()
 
-    async def add_item(self, id):
-        item = (await self.session.execute(select(models.Item).where(models.User.id == id))).one_or_none()[0]
-        proxy = ItemProxy(item)
-        proxy.sync_from()
-        self.pool[id] = proxy
+		async def add_item(self, id):
+				item = (await self.session.execute(select(models.Item).where(models.User.id == id))).one_or_none()[0]
+				proxy = ItemProxy(item)
+				proxy.sync_from()
+				self.pool[id] = proxy
 
-    async def save(self, id = None):
-        for i, j in self.pool.items():
-            j.sync_to()
-        self.session.commit()
+		async def save(self, id = None):
+				for i, j in self.pool.items():
+						j.sync_to()
+				self.session.commit()
 
 class PlayerPool:
-    pool = dict()
-    session = models.async_session()
+		pool = dict()
+		session = models.async_session()
 
-    async def add_item(self, dbid, poolid):
-        item = (await self.session.execute(select(models.Player).where(models.Player.id == dbid))).one_or_none()[0]
-        proxy = PlayerProxy(item)
-        proxy.sync_from()
-        self.pool[poolid] = proxy
+		async def add_item(self, dbid, poolid):
+				item = (await self.session.execute(select(models.Player).where(models.Player.id == dbid))).one_or_none()[0]
+				proxy = PlayerProxy(item)
+				proxy.sync_from()
+				self.pool[poolid] = proxy
 
-    async def save(self, id):
-        self.pool[id].sync_to()
-        await self.session.commit()
+		async def save(self, id):
+				self.pool[id].sync_to()
+				await self.session.commit()
 
 class StaticEntitiesPool:
-    pool = dict()
-    session = models.async_session()
+		pool = dict()
+		session = models.async_session()
 
-    async def add_item(self, id):
-        item = (await self.session.execute(select(models.StaticEntity).where(models.StaticEntity.id == id))).one_or_none()[0]
-        proxy = StaticEntityProxy(item)
-        proxy.sync_from()
-        self.pool[id] = proxy
+		async def add_item(self, id):
+				item = (await self.session.execute(select(models.StaticEntity).where(models.StaticEntity.id == id))).one_or_none()[0]
+				proxy = StaticEntityProxy(item)
+				proxy.sync_from()
+				self.pool[id] = proxy
 
-    async def save(self, id = None):
-        for i, j in self.pool.items():
-            j.sync_to()
-        self.session.commit()
+		async def save(self, id = None):
+				for i, j in self.pool.items():
+						j.sync_to()
+				self.session.commit()
