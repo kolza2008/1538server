15fb08c37479d0535f9b61c63ae27d5daaafcc39
kolza2008 <33565304+kolza2008@users.noreply.github.com>
2023-11-15 16:29:58 +0000

    possible postgress connect

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 PERLIN_SEED = 23454
 MAX_CHUNKS = 10
 CELL_STEP = 1 / (MAX_CHUNKS * 16)
-DATABASE_URL = "sqlite+aiosqlite:///database.db"
+DATABASE_URL = "sqlite+aiosqlite:///database.db" #"postgresql+asyncpg://onefivethreefourth_colorplate:a5da6c035d198b3e502bfee30067c8b8ee63b9a1@b4e.h.filess.io:5432/onefivethreefourth_colorplate"
