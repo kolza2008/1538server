8e4745a56e8cca50abb424d933a0fac836d2b788
Your Name <you@example.com>
2023-11-18 20:39:24 +0300

    new db address

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 PERLIN_SEED = 23454
 MAX_CHUNKS = 10
 CELL_STEP = 1 / (MAX_CHUNKS * 16)
-DATABASE_URL = "sqlite+aiosqlite:///database.db" #"postgresql+asyncpg://onefivethreefourth_colorplate:a5da6c035d198b3e502bfee30067c8b8ee63b9a1@b4e.h.filess.io:5432/onefivethreefourth_colorplate"
+DATABASE_URL = "postgresql+asyncpg://ljufnwqo:cZsX2Jc63SV9_on1_wgyXcilKIL4Wrb2@cornelius.db.elephantsql.com/ljufnwqo" #"sqlite+aiosqlite:///database.db"
