d11b645ef036adc2f3adfbc822f269df3805cd79
Your Name <you@example.com>
2023-11-19 00:40:43 +0300

    sqlite db

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 PERLIN_SEED = 23454
 MAX_CHUNKS = 10
 CELL_STEP = 1 / (MAX_CHUNKS * 16)
-DATABASE_URL = "postgresql+asyncpg://ljufnwqo:cZsX2Jc63SV9_on1_wgyXcilKIL4Wrb2@cornelius.db.elephantsql.com/ljufnwqo" #"sqlite+aiosqlite:///database.db"
+DATABASE_URL = "sqlite+aiosqlite:///database.db"
